380ea424e01927f176f3b6c4dcef1089336600c9
FelixTellmann <felix@tellmann.co.za>
2021-01-25 17:04:31 +0200

    Added .env keys

--- pages/api/product_update.tsx
+++ pages/api/product_update.tsx
@@ -2,12 +2,17 @@ import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-    console.log(req.body)
-  try {
-  
-  } catch (err) {
-    console.log(err.response.data)
+  const { retailer_id, payload } = JSON.parse(req.body)
+  console.log(JSON.parse(req.body))
+  if (retailer_id === process.env.VEND_RETAILER_ID) {
+    try {
+    
+    
+    
+    } catch (err) {
+      console.log(err.response.data)
+    }
+  } else {
+    res.status(401).json([]);
   }
-  
-  res.status(200).json('success');
 }
