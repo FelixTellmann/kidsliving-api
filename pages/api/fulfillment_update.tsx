979b6422239d5dd6e1bc5770c4c8c12b94522262
FelixTellmann <felix@tellmann.co.za>
2021-02-04 17:44:40 +0200

    Added .env keys

--- pages/api/fulfillment_update.tsx
+++ pages/api/fulfillment_update.tsx
@@ -25,6 +25,7 @@ type VariantSort = {
     status: string,
     quantity: number,
     index: number
+    needsProcessing: boolean
   }[]
 }
 
@@ -52,7 +53,6 @@ function getVendProductBySku(sku: string): AxiosPromise {
   });
 }
 
-
 export default (async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const shopifyWebhook = req.headers[`x-shopify-shop-domain`] === process.env.SHOPIFY_DOMAIN;
   let firebase = await loadFirebase();
@@ -61,81 +61,127 @@ export default (async (req: NextApiRequest, res: NextApiResponse): Promise<void>
   if (shopifyWebhook) {
     
     const { id: order_id, fulfillments, line_items } = req.body;
-    console.log(order_id);
-    
+    console.log(order_id, 'order_id');
     
     const { data: { fulfillment_orders } } = await getShopifyFulfillmentOrders(order_id);
-    const fulfillmentOrderIdArray = fulfillment_orders.map(({id})=> id )
-    let previouslyProcessed = []
+    const fulfillmentOrderIdArray = fulfillment_orders.map(({ id }) => id);
+    let previouslyProcessed = [];
     
-    await db.collection('fulfillment_orders').doc('' + order_id).get().then((result) => {
+    
+    fulfillment_orders.forEach(itm=>console.log(itm,','))
+    
+    
+    await db.collection("fulfillment_orders").doc("" + order_id).get().then((result) => {
       if (result.exists) {
-        previouslyProcessed = result.data().fulfillment_orders
+        previouslyProcessed = result.data().fulfillment_orders;
       }
-    })
-    
+    });
     
     if (isSameArray(fulfillmentOrderIdArray, previouslyProcessed)) {
-      console.log('no fulfillment_order change detected')
+      console.log("no fulfillment_order change detected");
       res.status(200).json("New Order / no fulfillment changes");
       return;
     }
-  
-  
+    
     const possibleChange = !!fulfillment_orders.filter(({ status }) => status === "closed").length;
     if (!possibleChange) {
-      await db.collection('fulfillment_orders').doc('' + order_id).set({fulfillment_orders: fulfillment_orders.map(({id})=> id )})
+      await db.collection("fulfillment_orders").doc("" + order_id).set({ fulfillment_orders: fulfillment_orders.map(({ id }) => id) });
       res.status(200).json("New Order / no fulfillment changes");
       return;
     }
-   
-    const sorted_fulfillment_orders: VariantSort = fulfillment_orders.reduce((acc, { id, assigned_location_id, status, line_items }, index) => {
+    
+    const sorted_fulfillment_orders: VariantSort = fulfillment_orders.reduce((
+      acc,
+      { id, assigned_location_id, status, line_items }
+    ) => {
       line_items.forEach(({ variant_id, quantity }) => {
         if (acc[variant_id]) {
-          acc[variant_id].push({ id, assigned_location_id, status, quantity, index, needsProcessing: !previouslyProcessed.includes(id) });
+          acc[variant_id].push({ id, assigned_location_id, status, quantity, needsProcessing: !previouslyProcessed.includes(id) });
         } else {
           acc[variant_id] = [];
-          acc[variant_id].push({ id, assigned_location_id, status, quantity, index, needsProcessing: !previouslyProcessed.includes(id) });
+          acc[variant_id].push({ id, assigned_location_id, status, quantity, needsProcessing: !previouslyProcessed.includes(id) });
         }
       });
       return acc;
     }, {});
     
-    const noFulfillmentChanges = Object.values(sorted_fulfillment_orders).every(item => {
-      return item.length === 1 && item[0].status === 'closed'
-    })
+    const sortedFilteredFulfillmentOrders = Object.entries(sorted_fulfillment_orders).reduce((acc, [key, val]) => {
+      if (val.some(({ needsProcessing }) => needsProcessing)) {
+        acc[key] = val.reduce((acc, itm) => {
+          if (!itm.needsProcessing) {
+            acc.lastProcessed = { ...itm };
+          } else {
+            if (!(acc.lastProcessed.quantity === itm.quantity && acc.lastProcessed.assigned_location_id ===
+              itm.assigned_location_id)) {
+              if (acc.lastProcessed.quantity !== itm.quantity) {
+                itm["updateQuantity"] = true;
+              }
+              acc.toProcess = {
+                ...itm,
+                updateQuantity: acc.lastProcessed.quantity !== itm.quantity,
+                updateLocation: acc.lastProcessed.assigned_location_id !== itm.assigned_location_id
+              };
+            }
+          }
+          
+          return acc;
+        }, {
+          lastProcessed: {
+            id: undefined,
+            assigned_location_id: undefined,
+            status: undefined,
+            quantity: undefined,
+            needsProcessing: undefined
+          },
+          toProcess: {
+            id: undefined,
+            assigned_location_id: undefined,
+            status: undefined,
+            quantity: undefined,
+            needsProcessing: undefined,
+            updateQuantity: false,
+            updateLocation: false
+          }
+        });
+      }
+      return acc;
+    }, {});
     
-    if (noFulfillmentChanges) {
+    console.log(sortedFilteredFulfillmentOrders);
+    
+    const noFulfillmentChanges = Object.values(sorted_fulfillment_orders).every(item => {
+      /*= =============== TODO: Might not be true based on processing final fulfillment ================ */
+      return item.length === 1 && item[0].status === "closed";
+    });
+    
+    /*if (noFulfillmentChanges) {
       res.status(200).json("Order already fulfilled");
       return;
-    }
-    
-    console.log(sorted_fulfillment_orders)
-    
+    }*/
     
     try {
-      const getShopifyVariantsByIdPromiseArr = []
-      Object.keys(sorted_fulfillment_orders).forEach((variant_id) => {
-        getShopifyVariantsByIdPromiseArr.push(getShopifyVariantById(+variant_id))
-      })
+      const getShopifyVariantsByIdPromiseArr = [];
+      Object.keys(sortedFilteredFulfillmentOrders).forEach((variant_id) => {
+        getShopifyVariantsByIdPromiseArr.push(getShopifyVariantById(+variant_id));
+      });
       
-      const variants = await Promise.all(catchErrors(getShopifyVariantsByIdPromiseArr))
+      const variants = await Promise.all(catchErrors(getShopifyVariantsByIdPromiseArr));
       
-      const getVendVariantsBySKUPromiseArr = []
-      variants.forEach(({data: { variant: { sku }}}) => {
-        getVendVariantsBySKUPromiseArr.push(getVendProductBySku(sku))
-      })
-  
-      const vendVariants = await Promise.all(catchErrors(getVendVariantsBySKUPromiseArr))
-      vendVariants.forEach(({data: {products}}) => {
-        console.log(products)
-      })
+      const getVendVariantsBySKUPromiseArr = [];
+      variants.forEach(({ data: { variant: { sku } } }) => {
+        getVendVariantsBySKUPromiseArr.push(getVendProductBySku(sku));
+      });
+      
+      const vendVariants = await Promise.all(catchErrors(getVendVariantsBySKUPromiseArr));
+      vendVariants.forEach(({ data: { products } }) => {
+        console.log(products);
+      });
       
     } catch (err) {
-      console.log(err.message)
+      console.log(err.message);
       res.status(200).json("error - getShopifyVariantById");
       return;
     }
   }
-  res.status(200).json('done');
+  res.status(200).json("done");
 });
