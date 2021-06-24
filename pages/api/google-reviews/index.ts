6edf53392cf8e5ff5e84537e99ab803139d4333f
FelixTellmann <felix@tellmann.co.za>
2021-06-24 11:00:30 +0200

    Added AWS Amplify for Cloud Functions

--- pages/api/google-reviews/index.ts
+++ pages/api/google-reviews/index.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-  const { id, name } = req.query;
-
-  res.setHeader("Access-Control-Allow-Credentials", "true");
+const allowCors = fn => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
@@ -13,7 +11,14 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
   );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   const response = await axios({
     method: "get",
     url: `https://maps.googleapis.com/maps/api/place/details/json?reference=ChIJ_8R_b09czB0RszEq5W8tTls&key=AIzaSyDVIMagdRaKg3l2-3DrLkr01kFBP9mPOSU`,
@@ -22,3 +27,5 @@ export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> 
   console.log(response);
   res.status(200).json(response?.data?.result);
 };
+
+export default allowCors(handler);
